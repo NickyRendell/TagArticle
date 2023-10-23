438b4428e2ec7abf99f57b373ee976bed35a83b5
Nick Rendell <nick.rendell@datacatalyst.io>
2023-10-23 22:11:17 +0200

    frustration

--- DbaseAccess.py
+++ DbaseAccess.py
@@ -1,7 +1,6 @@
 import requests
-import pandas as pd
-import json
 import re
+import json
 
 def clean_html(raw_html):
     """Function to clean HTML tags from a string."""
@@ -9,49 +8,36 @@ def clean_html(raw_html):
     cleantext = re.sub(cleanr, '', raw_html)
     return cleantext
 
-def get_wp_posts(endpoint="https://cphpost.dk/wp-json/wp/v2/posts", fields="id,excerpt,title", per_page=1):
-    page = 1
+def get_wp_posts(page=1, endpoint="https://cphpost.dk/wp-json/wp/v2/posts", fields="id,title,excerpt", per_page=10):
     all_posts = []
 
-    while True:
-        url = f"{endpoint}?_fields={fields}&per_page={per_page}&page={page}"
-        response = requests.get(url)
+    url = f"{endpoint}?_fields={fields}&per_page={per_page}&page={page}"
+    response = requests.get(url)
+    
+    # Check if the request was successful
+    if response.status_code != 200:
+        print(f"Request failed with status code: {response.status_code}")
+        return None
 
-        if response.status_code == 200:
-            try:
-                posts = response.json()
-            except json.JSONDecodeError:
-                print("The response is not in JSON format.")
-                print("Response text:", response.text)
-                break
-            
-            if not posts:
-                break  # Exit the loop if no more posts are returned
+    posts = response.json()
+    
+    for post in posts:
+        post_id = post.get('id')
+        title = post.get('title', {}).get('rendered', '')
+        excerpt = post.get('excerpt', {}).get('rendered', '')
+        excerpt = clean_html(excerpt).split('\n\n')[0]  # Clean HTML tags and get content up to the first two newlines
+        
+        all_posts.append({
+            'id': post_id,
+            'title': title,
+            'excerpt': excerpt
+        })
 
-            for post in posts:
-                raw_title = post.get("title", {}).get("rendered", "")
-                raw_excerpt = post.get("excerpt", {}).get("rendered", "")
-
-                clean_title = clean_html(raw_title).strip()
-                clean_excerpt = clean_html(raw_excerpt).strip().split("\n\n")[0]
-
-                extracted_data = {
-                    "id": post.get("id"),
-                    "title": clean_title,
-                    "excerpt": clean_excerpt
-                }
-                all_posts.append(extracted_data)
-
-            page += 1  # Go to the next page
-        else:
-            print(f"Failed to retrieve posts. HTTP Status Code: {response.status_code}")
-            print("Response text:", response.text)
-            break
-
-    df_posts = pd.DataFrame(all_posts)
-    return df_posts
+    return all_posts
 
 # Example usage
-df_posts = get_wp_posts()
-if not df_posts.empty:
-    print(df_posts)
+page_number = 1
+posts = get_wp_posts(page=page_number, per_page=5)
+if posts is not None:
+    for post in posts:
+        print(json.dumps(post, indent=2))

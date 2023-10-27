2c57ad08d00cd553d5194ca555a39a7a27ec5f4e
Nick Rendell <nick.rendell@datacatalyst.io>
2023-10-27 13:13:16 +0200

    potential main script now

--- testy.py
+++ testy.py
@@ -1,13 +1,61 @@
-def print_nearby_chars(text, position, radius=10):
-    start = max(0, position - radius)
-    end = min(len(text), position + radius + 1)
-    nearby_text = text[start:end]
-    
-    print("Context around position {}:".format(position))
-    print(nearby_text)
-    print(" " * (position - start) + "^-- position {}".format(position))
+import re
+import requests
+import pandas as pd
+import json
 
-# Example usage:
-json_text = """" [{"id":154547,"title":{"rendered":"Respected duo stand forward to lead Venstre"},"excerpt":{"rendered":"Troels Lund Poulsen and Stehpanie Lose are now official candidates to take the helm of Venstre. The duo is expected to be officially elected in November  \t\t<div class=\"woocommerce\">\n\t\t\t<div class=\"woocommerce-info wc-memberships-restriction-message wc-memberships-message wc-memberships-content-restricted-message\">\n\t\t\t\t\n<div class=\"wp-block-group paywall_login is-layout-flow wp-block-group-is-layout-flow\"><div class=\"wp-block-group__inner-container\">\n<hr class=\"wp-block-separator has-text-color has-black-color has-alpha-channel-opacity has-black-background-color has-background is-style-wide\" \/>\n\n\n\n<h2 class=\"wp-block-heading\">Full version of this article is only available to subscribers.<\/h2>\n\n\n\n<p><strong>Already a subscriber, sign in here:<\/strong><\/p>\n\n\n<div class=\"logged-out wp-block-button__link wp-element-button btn wp-block-loginout\"><a rel=\"nofollow\" href=\"https:\/\/cphpost.dk\/wp-login.php?redirect_to=https%3A%2F%2Fcphpost.dk%2Fwp-json%2Fwp%2Fv2%2Fposts%3Fpage%3D1%26_fields%3Did%2Ctitle%2Cexcerpt\">Log in<\/a><\/div>\n\n\n<div>\n\t<p class=\"woocommerce-LostPassword lost_password\">\n\t\t<a href=\"https:\/\/cphpost.dk\/my-account\/lost-password\/\">Lost your password?<\/a>\n\t<\/p>\n<\/div>\n\n<\/div><\/div>\n\n\n\n<hr class=\"wp-block-separator has-text-color has-black-color has-alpha-channel-opacity has-black-background-color has-background is-style-wide"""
-position = 221
-print_nearby_chars(json_text, position)
+# Define the API endpoint for the second page with the specific fields
+api_endpoint = "https://cphpost.dk/wp-json/wp/v2/posts?per_page=20&_fields=id,title,excerpt"
+
+# Make a GET request to the API
+response = requests.get(api_endpoint)
+
+data = response.text
+
+# Split the string into individual records
+records = re.split(r'(?=\{"id")', data)
+
+# Remove the first empty string in the list if it exists
+if records[0] == '':
+    records.pop(0)
+
+# Function to terminate the records
+def terminate_record(record):
+    # Terminate the record before "protected":false
+    if '"protected":false' in record:
+        record = record.split('"protected":false')[0]
+    # If <div class="woocommerce"> is in the string, terminate the record at that point
+    if r"""\t\t""" in record:
+        record = record.split(r"""\t\t""")[0]
+    return record
+
+# Terminate the records
+terminated_records = [terminate_record(record) for record in records]
+
+
+# Initialize an empty DataFrame
+df = pd.DataFrame(columns=["id", "title", "excerpt"])
+
+# Iterate through the records, parse them as JSON, and store the data in the DataFrame
+for i, record in enumerate(terminated_records, start=1):
+    # Add "}}" to the end of the record string
+    record += '"}}'
+    
+    try:
+        # Parse the string as a JSON object
+        record_json = json.loads(record)
+        
+        # Extract the id, title, and excerpt values
+        record_id = record_json.get("id", "")
+        title = record_json.get("title", {}).get("rendered", "")
+        excerpt = record_json.get("excerpt", {}).get("rendered", "")
+        
+        # Add the data to the DataFrame
+        df = df._append({"id": record_id, "title": title, "excerpt": excerpt}, ignore_index=True)
+        
+        # Print the record
+        print(f"Record {i}:\nID: {record_id}\nTitle: {title}\nExcerpt: {excerpt}\n")
+    except json.JSONDecodeError as e:
+        print(f"Record {i} could not be parsed as JSON: {e}")
+
+# Print the DataFrame
+print(df)

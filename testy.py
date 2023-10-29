958c322a0f68fbef6b87f214a16f763a6987bc18
Nick Rendell <nick.rendell@datacatalyst.io>
2023-10-29 17:26:38 +0100

    changes

--- testy.py
+++ testy.py
@@ -2,9 +2,12 @@ import re
 import requests
 import pandas as pd
 import json
+import html
+from bench_test import categorise_title_excerpt
+import time
 
 # Define the API endpoint for the second page with the specific fields
-api_endpoint = "https://cphpost.dk/wp-json/wp/v2/posts?per_page=20&_fields=id,title,excerpt"
+api_endpoint = "https://cphpost.dk/wp-json/wp/v2/posts?per_page=50&page=1&_fields=id,title,excerpt"
 
 # Make a GET request to the API
 response = requests.get(api_endpoint)
@@ -22,7 +25,7 @@ if records[0] == '':
 def terminate_record(record):
     # Terminate the record before "protected":false
     if '"protected":false' in record:
-        record = record.split('"protected":false')[0]
+        record = record.split('","protected":false')[0]
     # If <div class="woocommerce"> is in the string, terminate the record at that point
     if r"""\t\t""" in record:
         record = record.split(r"""\t\t""")[0]
@@ -39,6 +42,7 @@ df = pd.DataFrame(columns=["id", "title", "excerpt"])
 for i, record in enumerate(terminated_records, start=1):
     # Add "}}" to the end of the record string
     record += '"}}'
+    print (record)  
     
     try:
         # Parse the string as a JSON object
@@ -57,5 +61,34 @@ for i, record in enumerate(terminated_records, start=1):
     except json.JSONDecodeError as e:
         print(f"Record {i} could not be parsed as JSON: {e}")
 
+def unescape_html_entities(text):
+    return html.unescape(text)
+
+df['title'] = df['title'].apply(unescape_html_entities)
+df['excerpt'] = df['excerpt'].apply(unescape_html_entities)
+
 # Print the DataFrame
-print(df)
+
+#save to new df
+#df.to_csv('CPHPost.csv', index=False)
+
+df.to_csv('filename.csv', encoding='utf-8-sig', index=False)
+
+#create a new column in dataframe called prediction
+df['prediction'] = ""
+
+#set up loop that concantenates the title and excerpt with '-' in between then sends the string to the function categorise_title_excerpt
+#the function returns a prediction which is then added to the prediction column in the dataframe
+for i in range(len(df)):
+    title = df['title'][i]
+    excerpt = df['excerpt'][i]
+    title_excerpt = title + ' - ' + excerpt
+    #wait 10 seconds
+    time.sleep(10)
+    prediction = categorise_title_excerpt(title_excerpt)
+    df['prediction'][i] = prediction
+
+#save dataframe to csv with utf-8string
+#  encoding
+df.to_csv('CPHPostA.csv', encoding='utf-8-sig', index=False)
+
